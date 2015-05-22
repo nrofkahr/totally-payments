98c3803bea32c5ecca274bdbc27da4d7e6a0f770
Nathan Rofkahr <nathan.rofkahr@gmail.com>
2015-05-21 23:04:21 -0500

    Implemented product list page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-
-
 growth_hacking_monthly = Product.create(
   title: "Growth Hacking Monthly", 
   subtitle: "Ongoing Course", 
   author: "Nate", 
-  price: "4.99", 
+  price: "9.00", 
   sku: "GROHACK2", 
   download_url: "https://s3.amazonaws.com/totally-payments/LeanMarketingFramework.png",
   details: "We'll charge you monthly for unlimited access to this course",
@@ -25,9 +23,57 @@ growth_hacking_monthly = Product.create(
                 <li>How to apply the Lean Marketing Framework</li>
                 <li>Resources and tools you'll need to know</li>
             </ul>,}, 
-  author_description: %{ <p>Hey, I'm Nate, co-founder of <a href="http://www.onemonth.com" target="_blank">StartupFormulas</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+  author_description: %{ <p>Hey, I'm Nate, co-founder of <a href="http://www.startupformulas.com" target="_blank">StartupFormulas</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
                 <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
                 </p>
-                <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>}, 
+                <p>Follow me on Twitter <a href="https://twitter.com/rofkahr" target="_blank">@rofkahr</a></p>}, 
   length: "6 months", 
   author_image_name: "teacher-image.jpg",)
+
+
+growth_hacking_monthly = Product.create(
+  title: "Copywriting Monthly", 
+  subtitle: "Ongoing Course", 
+  author: "Nate", 
+  price: "9.00", 
+  sku: "COPYWRITE2", 
+  download_url: "https://s3.amazonaws.com/totally-payments/LeanMarketingFramework.png",
+  details: "We'll charge you monthly for unlimited access to this course",
+  description: %{<p>In this series, Nate introduces you to the concept of Copywriting and shares his favorite tips for getting started as a Copywriter.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>What is a Copywriter?</li>
+                <li>The 5 secrets of writing for $$</li>
+                <li>How to tell a story </li>
+                <li>Resources and tools you'll need to know</li>
+            </ul>,}, 
+  author_description: %{ <p>Hey, I'm Nate, co-founder of <a href="http://www.startupformulas.com" target="_blank">StartupFormulas</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/rofkahr" target="_blank">@rofkahr</a></p>}, 
+  length: "6 months", 
+  author_image_name: "teacher-image.jpg",)
+
+analytics_monthly = Product.create(
+  title: "Copywriting Monthly", 
+  subtitle: "Ongoing Course", 
+  author: "Nate", 
+  price: "9.00", 
+  sku: "ANALYTICS2", 
+  download_url: "https://s3.amazonaws.com/totally-payments/LeanMarketingFramework.png",
+  details: "We'll charge you monthly for unlimited access to this course",
+  description: %{<p>In this series, Nate introduces you to Mixpanel and shares his favorite tips for getting started with Mixpanel.</p>
+            <p><strong>What You'll Learn</strong></p>
+            <ul class="no-indent">
+                <li>How to use Mixpanel</li>
+                <li>How to track and improve your funnel</li>
+                <li>How to get free stuff</li>
+                <li>Resources and tools you'll need to know</li>
+            </ul>,}, 
+  author_description: %{ <p>Hey, I'm Nate, co-founder of <a href="http://www.startupformulas.com" target="_blank">StartupFormulas</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+                <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+                </p>
+                <p>Follow me on Twitter <a href="https://twitter.com/rofkahr" target="_blank">@rofkahr</a></p>}, 
+  length: "6 months", 
+  author_image_name: "teacher-image.jpg",)
+
